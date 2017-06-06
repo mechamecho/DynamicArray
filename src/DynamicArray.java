b2d25efeb467ff80f69737d75ac3d52f931083ab
Livepurposefully <nimiko_chan@live.com>
2017-06-05 23:40:54 -0400

    Dynamic Array Problem solved, need to resolve the issue with having to press ENTER to get the second number

--- src/DynamicArray.java
+++ src/DynamicArray.java
@@ -1,39 +1,46 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-/**
- * Created by Engineer on 6/5/2017.
- */
+
 public class DynamicArray {
-    public static void Solution(String [] args){
 
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner in= new Scanner(System.in);
         int n=in.nextInt();
         int bigq=in.nextInt();
-        in.nextLine();
         int lastAnswer=0;
-        List<List<Integer>>seqList=new ArrayList<>(n);//initiate size of the bigger list to n, for
-        //n sequences
-        /*
-        don't know how to create a list of lists, if I create a 2 dimensional array, I will have trouble
-        appending things to different sized arrays
-         */
+        ArrayList<Integer>[] seqList = new ArrayList[n];
+
+        for (int i=0;i<n;i++){
+            seqList[i]=new ArrayList<Integer>();
+
+        }
+
+
+
         for (int i=0;i<bigq;i++){
             int q=in.nextInt();
             int x=in.nextInt();
             int y=in.nextInt();
-            in.nextLine();
+
             int index=((x^lastAnswer)%n);
-            List seq=seqList.get(index);
+            ArrayList<Integer> seq=seqList[index];
+
             if(q==1){
+
                 seq.add(y);
             }
             else{
-                lastAnswer=(y%seq.size());
-                System.out.print(System.lineSeparator());
-                System.out.print(lastAnswer);
+                lastAnswer=(seq.get(y%(seq.size())));
+                System.out.print(System.lineSeparator()+lastAnswer);
+
             }
+
+
         }
     }
 }
